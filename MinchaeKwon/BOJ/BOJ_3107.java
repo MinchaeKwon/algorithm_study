76f2c01d7d9a0da409a2deee63459dd363e565f3
권민채 <61741340+MinchaeKwon@users.noreply.github.com>
2024-01-29 17:33:04 +0900

    IPv6 / 40분

--- MinchaeKwon/BOJ/BOJ_3107.java
+++ MinchaeKwon/BOJ/BOJ_3107.java
@@ -1,3 +1,37 @@
+/**
+ * 3107 IPv6
+ * https://www.acmicpc.net/problem/3107
+ * 
+ * @author minchae
+ * @date 2024. 1. 29.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		
+		
+		// 0으로만 이루어진 그룹이 있을 경우 구분하기 위해 다른 문자열을 넣음
+		if (input.contains("::")) {
+			input = input.replace("::", ":zero:");
+		}
+		
+		String[] arr = input.split(":");
+		ArrayList<String> ipv6 = new ArrayList<>();
+		
+		for (String str : arr) {
+			// ::이 맨 앞에 있을 경우 배열 첫 번째는 비어있기 때문에 다음으로 넘어감
+			if (str.isEmpty()) {
+				continue;
 			}
 			
 			// 길이가 4미만일 경우 앞에 0을 붙임
